90d81c88640a19beee7210ebddcfc397954c5039
Shadae Holmes <ShadaeHolmes@gmail.com>
2013-11-08 12:22:26 -0800

    teen update to silence command

--- teen.rb
+++ teen.rb
@@ -1,9 +1,28 @@
 class Teen
+  
+  def initialize
+    puts "What do you want to ask our Timmy?"
+    message = gets.chomp
+    evaluate_message
+  end
 
-	def initialize
-		
-	end
+  def evaluate_message
+    if  #yell method returns true boolean
 
+    elsif #question method returns true boolean
+                        
+    elsif #silence method returns true boolean
+
+    else
+
+    end
+  end
+
+  def yell
+    if message == message.upcase
+      puts "Timmy says \"Woah, chill out!\""
+      end
+    end
 
   def question
     if message.include? "?"
@@ -12,6 +31,11 @@ class Teen
 
   end
 
+  def silence
+    if message == nil || message == "..." || message == " "
+      puts "Whatever!"
+    end              
+  end
 end
 
 
