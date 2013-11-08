a98705ed1137d28880aa6beca39adb0d5055ed6c
Whitney Levis <ms.whitney.levis@gmail.com>
2013-11-08 12:06:22 -0800

    Reorganize methods to booleans and add final else

--- teen.rb
+++ teen.rb
@@ -1,37 +1,41 @@
 class Teen
 
-	def initialize
-		puts "What do you want to ask our Timmy?"
-		message = gets.chomp
-		evaluate_message
-	end
+  def initialize
+    puts "What do you want to ask our Timmy?"
+    message = gets.chomp
+    evaluate_message
+  end
 
-	def evaluate_message
-		if  #yell method returns true boolean
-
-		elsif #question method returns true boolean
-			
-		elsif #silence method returns true boolean
-
-		else
-
-		end
-	end
-
-	def yell
-		if message == message.upcase
-			puts "Timmy says \"Woah, chill out!\""
-		end
-	end
-
-	def question
-    if message.include? "?"
+  def evaluate_message
+    if yell #yell method returns true boolean
+      puts "Woah, chill out!"
+    elsif question #question method returns true boolean
       puts "Sure..... "
+    elsif #silence method returns true boolean
+
+    else
+      puts "Whatever"
+    end
+  end
+
+  def yell
+    if message == message.upcase
+      true
+    else
+      false
+    end
+  end
+
+  def question
+    if message.include? "?"
+      true
+    else
+      false
     end
 
   end
 
-	def silence
-		
-	end
+  def silence
+    
+  end
 end
